47e8f7564bf4b039b8331bcb202579eae26418c7
Björn Erlwein <bjoern@bjoernerlwein.de>
2022-03-19 13:57:31 +0100

    Add rayon

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.2.0
+
+Added [rayon](https://docs.rs/rayon/latest/rayon/) for parallel iterators whenever possible. This most likely doesn't really do much of anything but it is faster on my machine when running the command 1000 times so why not.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rayon::prelude::*;
 use std::{
     env, fs,
     io::{BufWriter, Write},
@@ -9,38 +10,56 @@ use std::{
 fn visit_path_entry(path_entry: &str) -> Vec<String> {
     let path = Path::new(path_entry);
 
-    let mut result: Vec<String> = Vec::new();
+    let empty: Vec<String> = Vec::new();
     eprintln!("Checking {}", path_entry);
     if path.is_dir() {
         match fs::read_dir(path) {
             Ok(entries) => {
-                for dir_entry in entries {
-                    match dir_entry {
+                let collected: Vec<_> = entries.collect();
+
+                let result: Vec<String> = collected
+                    .par_iter()
+                    .filter_map(|dir_entry| match dir_entry {
                         Ok(entry) => {
                             if entry.path().is_file() {
                                 match fs::metadata(entry.path()) {
                                     Ok(metadata) => {
                                         if metadata.permissions().mode() & 0o111 != 0 {
                                             match entry.path().to_str() {
-                                                Some(x) => result.push(x.to_string()),
-                                                None => (),
+                                                Some(s) => Some(s.to_string()),
+                                                None => None,
                                             }
+                                        } else {
+                                            None
                                         }
                                     }
-                                    Err(e) => eprintln!("{}", e),
+                                    Err(e) => {
+                                        eprintln!("{}", e);
+                                        None
+                                    }
                                 }
+                            } else {
+                                None
                             }
                         }
-                        Err(e) => eprintln!("{}", e),
-                    }
-                }
+                        Err(e) => {
+                            eprintln!("{}", e);
+                            None
+                        }
+                    })
+                    .map(|entry| entry.to_string())
+                    .collect();
+                result
             }
-            Err(_e) => eprintln!("Failed to read from {}", path_entry),
+            Err(_e) => {
+                eprintln!("Failed to read from {}", path_entry);
+                empty
+            }
         }
     } else {
         eprintln!("{} is not a directory, skipping...", path_entry);
+        empty
     }
-    result
 }
 
 fn main() {
@@ -49,8 +68,11 @@ fn main() {
         Ok(path) => {
             eprintln!("got {}", path);
 
-            let split = path.split(":");
-            let collected = split.map(|path_entry| visit_path_entry(path_entry));
+            let split: Vec<&str> = path.split(":").collect();
+            let collected: Vec<_> = split
+                .par_iter()
+                .map(|path_entry| visit_path_entry(path_entry))
+                .collect();
 
             let mut executables: Vec<String> = Vec::new();
 
@@ -61,10 +83,10 @@ fn main() {
             }
 
             let mut binaries: Vec<&str> = executables
-                .iter()
+                .par_iter()
                 .filter_map(|executable| executable.split("/").last())
                 .collect();
-            binaries.sort();
+            binaries.par_sort();
 
             let stdout = std::io::stdout();
             let lock = stdout.lock();
